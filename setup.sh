f078c84cc417296463e64c39a02945642ea875b7
Kendrick Martin <kmartin@crowdcompass.com>
2015-01-17 09:22:25 -0800

    Changing case to match others

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ curl -X PUT http://docker:5984/config
 curl -X PUT http://docker:5984/config/bbbd5b349b7aa42d7cf0c38dd7028812 -d '{"name":"The Hop and Vine", "twitter":"thehopandvine" }'
 curl -X PUT http://docker:5984/config/bbbd5b349b7aa42d7cf0c38dd7028652 -d '{"name":"Hopworks", "twitter":"hopworksbeer" }'
 curl -X PUT http://docker:5984/config/bbbd5b349b7aa42d7cf0c38dd70278f4 -d '{"name":"The Beermongers", "twitter":"thebeermongers" }'
-curl -X PUT http://docker:5984/config/bbbd5b349b7aa42d7cf0c38dd702737d -d '{"name":"Saraveza", "twitter":"Saraveza" }'
+curl -X PUT http://docker:5984/config/bbbd5b349b7aa42d7cf0c38dd702737d -d '{"name":"Saraveza", "twitter":"saraveza" }'
 docker attach $DOCKERID
