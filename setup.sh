4e8cbd0a665b9810309d6b4111455f944eff91bf
Kendrick Martin <kmartin@crowdcompass.com>
2015-03-19 09:49:42 -0700

    Adding seed data and changing setup to only start the db

--- setup.sh
+++ setup.sh
@@ -1,8 +1,7 @@
 DOCKERID=$(docker run -d -p 5984:5984 klaemo/couchdb)
 sleep 10
-curl -X PUT http://docker:5984/config
-curl -X PUT http://docker:5984/config/bbbd5b349b7aa42d7cf0c38dd7028812 -d '{"name":"The Hop and Vine", "twitter":"thehopandvine" }'
-curl -X PUT http://docker:5984/config/bbbd5b349b7aa42d7cf0c38dd7028652 -d '{"name":"Hopworks", "twitter":"hopworksbeer" }'
-curl -X PUT http://docker:5984/config/bbbd5b349b7aa42d7cf0c38dd70278f4 -d '{"name":"The Beermongers", "twitter":"thebeermongers" }'
-curl -X PUT http://docker:5984/config/bbbd5b349b7aa42d7cf0c38dd702737d -d '{"name":"Saraveza", "twitter":"saraveza" }'
+#curl -X PUT http://docker:5984/config/bbbd5b349b7aa42d7cf0c38dd7028812 -d '{"name":"The Hop and Vine", "twitter":"thehopandvine" }'
+#curl -X PUT http://docker:5984/config/bbbd5b349b7aa42d7cf0c38dd7028652 -d '{"name":"Hopworks", "twitter":"hopworksbeer" }'
+#curl -X PUT http://docker:5984/config/bbbd5b349b7aa42d7cf0c38dd70278f4 -d '{"name":"The Beermongers", "twitter":"thebeermongers" }'
+#curl -X PUT http://docker:5984/config/bbbd5b349b7aa42d7cf0c38dd702737d -d '{"name":"Saraveza", "twitter":"saraveza" }'
 docker attach $DOCKERID
